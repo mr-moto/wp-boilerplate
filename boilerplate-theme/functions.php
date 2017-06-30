6b81de4cbfd364f081e6b45c215371e6fdfdaf19
mr-moto <khash615@gmail.com>
2017-06-29 21:30:55 -0700

    added useful scripts to functions.php

--- boilerplate-theme/functions.php
+++ boilerplate-theme/functions.php
@@ -85,8 +85,11 @@ add_filter( 'stylesheet_uri', 'dummy_theme_name_minified_css', 10, 2 );
 function dummy_theme_name_scripts() {
 	wp_enqueue_style( 'dummy-theme-name-style', get_stylesheet_uri() );
 
+	wp_enqueue_script('dummy-theme-name-main', get_template_directory_uri() . '/build/js/main.min.js', array( 'jquery' ), '1.0.0', true );
 	wp_enqueue_script( 'dummy-theme-name-skip-link-focus-fix', get_template_directory_uri() . '/build/js/skip-link-focus-fix.min.js', array(), '20130115', true );
 
+	wp_enqueue_style( 'fontAwesome', 'https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css', array(), '4.4.0');
+	
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
 	}
